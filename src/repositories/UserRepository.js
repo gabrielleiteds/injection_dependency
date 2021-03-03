744eb5694fcacd3b3787c82099a2fbc98d589a46
leitegs <leitedasilvag@gmail.com>
2021-03-03 12:15:19 -0300

    Implementando o repositório de usuário

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,39 @@
+const UserModel = require('../models/User');
+
+class UserRepository {
+  constructor() {
+    this.model = UserModel;
+  }
+
+  async create(user) {
+    return this.model.create(user);
+  }
+
+  async getUser(email) {
+    const checkUser = await this.model.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return checkUser;
+  }
+
+  async update(fields) {
+    return this.model.update(fields, {
+      where: {
+        id: fields.id,
+      },
+    });
+  }
+
+  async delete(userId) {
+    return this.model.destroy({
+      where: {
+        id: userId,
+      },
+    });
+  }
+}
+
+module.exports = new UserRepository();
