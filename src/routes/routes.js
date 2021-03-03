acca43fbe1135ef76913f59364507e006dde1ef0
leitegs <leitedasilvag@gmail.com>
2021-03-03 13:18:09 -0300

    centralizando e adicionando prefix as rotas do usuario

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
 
+const userRoutes = require('../modules/users/userRoutes');
+
 const routes = Router();
+
+routes.use('/user', userRoutes);
 
 routes.get('/', (req, res) => {
   res.json({ statusCode: '200' });
