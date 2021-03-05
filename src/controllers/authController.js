6af4d1e3be78513b218c801fffffdcca6ef2b888
leitegs <leitedasilvag@gmail.com>
2021-03-05 11:48:21 -0300

    refat: mudando maneira de exportar arquivo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,15 @@
-const { request, response } = require('express');
 const AuthService = require('../modules/users/services/authService');
 
-class AuthController {
-  constructor() {
-    this.req = request;
-    this.res = response;
-  }
-
-  async authenticate() {
-    const { email, password } = this.req.body;
+module.exports = {
+  async authenticate(req, res) {
+    const { email, password } = req.body;
 
     const token = await AuthService.authenticate(email, password);
 
     if (!token) {
-      return this.res.status(500).json();
+      return res.status(500).json();
     }
 
-    return this.res.status(200).cookie('authorization', token).json({ token });
-  }
-}
-
-module.exports = new AuthController();
+    return res.status(200).cookie('authorization', token).json({ token });
+  },
+};
